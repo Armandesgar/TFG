6632f362b38d0778c1288231edd4689b52c24b8f
Armandesgar <67513892+Armandesgar@users.noreply.github.com>
2020-06-28 00:46:24 +0200

    Update excel_complert.m

--- excel_complert.m
+++ excel_complert.m
@@ -1,10 +1,11 @@
 function excel_complert(nomcsv,nomcsv_linees,nom_excel,nomimatgea,nomimatgep,nomcsvrota)
+%Iniciem llegint i fent transformacions de coordenades
 [nas,adh,potaE,potaD,potae,potad,cua,ficua]=llegir_dades(nomcsv);
 [x1,x2,x3,x4]=punts_linees(nomcsv_linees);
 C=matriu_transformacio(x1,x2,x3,x4);
 [nasX,nasY,adhX,adhY,potaEX,potaEY,potaDX,potaDY,potaeX,potaeY,potadX,potadY,cuaX,cuaY,ficuaX,ficuaY]=canvi_a_cm(C,nas,adh,potaE,potaD,potae,potad,cua,ficua);
 time=(1:length(nasX))/25;
-%neteja dades
+%Eliminació de pics
 nasX=neteja_dades(nasX);        nasY=neteja_dades(nasY);
 adhX=neteja_dades(adhX);        adhY=neteja_dades(adhY);
 potaDX=neteja_dades(potaDX);    potaEY=neteja_dades(potaEY);
@@ -13,7 +14,7 @@ potadX=neteja_dades(potadX);    potadY=neteja_dades(potadY);
 potaeX=neteja_dades(potaeX);    potaeY=neteja_dades(potaeY);
 cuaX=neteja_dades(cuaX);        cuaY=neteja_dades(cuaY);
 ficuaX=neteja_dades(ficuaX);    ficuaY=neteja_dades(ficuaY);
-%filtre
+%filtre 5 punts
 nasX=filtre_5_punts(nasX);      nasY=filtre_5_punts(nasY);
 adhX=filtre_5_punts(adhX);      adhY=filtre_5_punts(adhY);
 potaDX=filtre_5_punts(potaDX);  potaEY=filtre_5_punts(potaEY);
@@ -22,7 +23,7 @@ potadX=filtre_5_punts(potadX);  potadY=filtre_5_punts(potadY);
 potaeX=filtre_5_punts(potaeX);  potaeY=filtre_5_punts(potaeY);
 cuaX=filtre_5_punts(cuaX);      cuaY=filtre_5_punts(cuaY);
 ficuaX=filtre_5_punts(ficuaX);  ficuaY=filtre_5_punts(ficuaY);
-%C�lcul de velocitats
+%Cŕlcul de velocitats
 CENTREX=(potaEX+potaDX+potaeX+potadX)/4;
 CENTREY=(potaEY+potaDY+potaeY+potadY)/4;
 Modul_velocitat=sqrt(((CENTREX(2:end)-CENTREX(1:end-1))*25).^2+((CENTREY(2:end)-CENTREY(1:end-1))*25).^2);
@@ -41,16 +42,16 @@ velocitatpotaD=sqrt(((potaDX(2:end)-potaDX(1:end-1))*25).^2+((potaDY(2:end)-pota
 velocitatpotae=sqrt(((potaeX(2:end)-potaeX(1:end-1))*25).^2+((potaeY(2:end)-potaeY(1:end-1))*25).^2);
 velocitatpotad=sqrt(((potadX(2:end)-potadX(1:end-1))*25).^2+((potadY(2:end)-potadY(1:end-1))*25).^2);
 velocitatcua=sqrt(((cuaX(2:end)-cuaX(1:end-1))*25).^2+((cuaY(2:end)-cuaY(1:end-1))*25).^2);
-%C�lcul de velocitats
+%Cŕlcul de distancies
 distanciamorro=sqrt((nasX-adhX).^2+(nasY-adhY).^2);
 distanciapotes=sqrt((potaEX-potaDX).^2+(potaEY-potaDY).^2);
 distanciaadhD=sqrt((potaDX-adhX).^2+(potaDY-adhY).^2);
 distanciaadhE=sqrt((potaEX-adhX).^2+(potaEY-adhY).^2);
 
-%Ictus
+%Resultats Tape-Removal
 [Deteccio,Treu]=tape_removal(nomcsv,nomcsv_linees);
 
-%Propietats
+%Altres Propietats
 x=Modul_velocitat>1;
 temps_quiet=sum(x)/length(Modul_velocitat);
 Velocitat_mitjana=Modul_velocitat(x);   velocitat_mitjana=mean(Velocitat_mitjana);
@@ -77,7 +78,7 @@ angle_mig=mean(a);
 temps_angle_0positiu=sum(a>0)/length(a);
 temps_angle_0negatiu=1-temps_angle_0positiu;
 
-%Imatges
+%Càlcul regioó infartada (Imatges)
 if nomimatgea==0 | nomimatgep==0
     AreaInfartada1=0;   AreaInfartada1V=0;
     AreaInfartada2=0;   AreaInfartada2V=0;
@@ -161,7 +162,7 @@ VolumContralateralV=((AreaContrammV1+AreaContrammV2)*0.15+0.1*(AreaContrammV3+Ar
 VolumInfart_corregit_edemaV=VolumInfartatV*VolumContralateralV/VolumIpsilateralV;
 Edemacervell_sencerV=((VolumIpsilateralV-VolumContralateralV)/(VolumIpsilateralV+VolumContralateralV))*100;
 
-% Rotarot
+%Resultats Rotarot
 numero=split(nomcsv,'-');   numero=numero(1);       numero=string(numero);
 numero=str2num(numero);     numero=numero-601;      numero=mod(numero,4);   
 if numero==0
@@ -170,5 +171,5 @@ end
 TEMPS1=rotarot(nomcsvrota);
 Temps=TEMPS1(numero);
 
-%modificar_excel(nom_excel,ceil(Treu*4)/4,ceil(Deteccio*4)/4,nomcsv)
+%Fiquem les dades a l'excel
 excel_propietats(nom_excel,nomcsv,AreaInfartada1,AreaInfartada2,AreaInfartada3,AreaInfartada4,AreaInfartada5,AreaInfartada6,AreaInfartada7,AreaInfartada8,AreaInfartadamm1,AreaInfartadamm2,AreaInfartadamm3,AreaInfartadamm4,AreaInfartadamm5,AreaInfartadamm6,AreaInfartadamm7,AreaInfartadamm8,Rati1,Rati2,Rati3,Rati4,Rati5,Rati6,Rati7,Rati8,AreaIpsi1,AreaIpsi2,AreaIpsi3,AreaIpsi4,AreaIpsi5,AreaIpsi6,AreaIpsi7,AreaIpsi8,AreaIpsimm1,AreaIpsimm2,AreaIpsimm3,AreaIpsimm4,AreaIpsimm5,AreaIpsimm6,AreaIpsimm7,AreaIpsimm8,AreaContra1,AreaContra2,AreaContra3,AreaContra4,AreaContra5,AreaContra6,AreaContra7,AreaContra8,AreaContramm1,AreaContramm2,AreaContramm3,AreaContramm4,AreaContramm5,AreaContramm6,AreaContramm7,AreaContramm8,VolumInfartat,VolumIpsilateral,VolumContralateral,VolumInfart_corregit_edema,Edemacervell_sencer,AreaInfartada1V,AreaInfartada2V,AreaInfartada3V,AreaInfartada4V,AreaInfartada5V,AreaInfartada6V,AreaInfartada7V,AreaInfartada8V,AreaInfartadamm1V,AreaInfartadamm2V,AreaInfartadamm3V,AreaInfartadamm4V,AreaInfartadamm5V,AreaInfartadamm6V,AreaInfartadamm7V,AreaInfartadamm8V,Rati1V,Rati2V,Rati3V,Rati4V,Rati5V,Rati6V,Rati7V,Rati8V,AreaIpsiV1,AreaIpsiV2,AreaIpsiV3,AreaIpsiV4,AreaIpsiV5,AreaIpsiV6,AreaIpsiV7,AreaIpsiV8,AreaIpsimmV1,AreaIpsimmV2,AreaIpsimmV3,AreaIpsimmV4,AreaIpsimmV5,AreaIpsimmV6,AreaIpsimmV7,AreaIpsimmV8,AreaContraV1,AreaContraV2,AreaContraV3,AreaContraV4,AreaContraV5,AreaContraV6,AreaContraV7,AreaContraV8,AreaContrammV1,AreaContrammV2,AreaContrammV3,AreaContrammV4,AreaContrammV5,AreaContrammV6,AreaContrammV7,AreaContrammV8,VolumInfartatV,VolumIpsilateralV,VolumContralateralV,VolumInfart_corregit_edemaV,Edemacervell_sencerV,Deteccio,Treu,temps_quiet,velocitat_mitjana,velocitat_morro,velocitat_potaD,velocitat_potaE,velocitat_potad,velocitat_potae,velocitat_cul,distancia_potes_mig,distancia_adh_morro,angle_mig,temps_angle_0positiu,temps_angle_0negatiu,Temps)
